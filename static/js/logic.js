9d6a10fe3a452e4206d1b321650b2a199737e72e
KLacerda08 <kaldmr7880@gmail.com>
2021-02-09 17:11:42 -0500

    worked on code

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,10 +4,120 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function(record) {
   // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+  createFeatures(record.features);
+  console.log(record.features);
 });
+
+// Get magnitudes 
+d3.json(queryUrl, function(record) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  // createMag(data.features.properties.magnitude);
+  
+  // -- this prints the first of the 2181 objects/elements in the Features array.
+  console.log(record.features[0]); 
+  // -- this prints the id of the first of the 2181 objects/elements in the Features array.
+  console.log(record.features[0].id);
+  // -- this prints the the properties  of the first of the 2181 objects/elements in the Features array.
+  console.log(record.features[0].properties)
+  // Now need to get to a key value for magnitude inside the properties - calling the value from the dictionary key - YAY!! 
+  console.log(record.features[0].properties["mag"])
+  // Now need to get to a key value for EQ depth inside the geometry - calling the value from the dictionary key - YAY!! 
+  console.log(record.features[0].geometry["coordinates"][2])
+
+
+  // Assign color to EQ based on depth)
+  // first, get list of depths to get an idea of the range. Per USGS, depth measured in kilometers 
+  // but the reference point is variable so anyone viewing this visualization should be careful!  
+  // It may not be as it seems.   
+  depths = []
+  for (var i = 0; i < record.features.length; i++) {
+    // console.log(Math.max(record.features[i].geometry["coordinates"][2]))
+   if (record.features[i].geometry["coordinates"][2] !== null) {
+    depths.push(parseInt(record.features[i].geometry["coordinates"][2]))
+   }
+  }
+  console.log(Math.max.apply(Math, depths))
+  console.log(Math.min.apply(Math, depths))
+  });
+  // Min = -4, Max = 626.  Set Range -50 to 1000, since the data is updated every minute, give it some slack.   
+
+  
+
+//   for (var i = 0; i < record.features.length; i++) {
+//     if (record.features[i].properties["mag"] > 10) {
+//        color = "DeepPink"
+//     }
+//     else if (record.features[i].properties["mag"] > 9) {
+//       color = "DarkMagenta"
+//     }
+//     else if (record.features[i].properties["mag"] > 8) {
+//       color = "Crimson"
+//     }
+//     else if (record.features[i].properties["mag"] > 7) {
+//       color = "Coral"
+//     }
+//     else if (record.features[i].properties["mag"] > 6) {
+//       color = "DarkOrange"
+//     }
+//     else if (record.features[i].properties["mag"] > 5) {
+//       color = "GoldenRod"
+//     }
+//     else if (record.features[i].properties["mag"] > 4) {
+//       color = "Gold"
+//     }
+//     else if (record.features[i].properties["mag"] > 3) {
+//       color = "YellowGreen"
+//     }
+//     else if (record.features[i].properties["mag"] > 2) {
+//       color = "LimeGreen"
+//     }
+//     else if (record.features[i].properties["mag"] > 1) {
+//       color = "MediumTurquoise"
+//     }
+//     else 
+//       color = "DodgerBlue"
+//     }
+// });
+
+//   // Assign color to magnitudes based on richter scale (0 to greater than 10)
+//   for (var i = 0; i < record.features.length; i++) {
+//         if (record.features[i].properties["mag"] > 10) {
+//            color = "DeepPink"
+//         }
+//         else if (record.features[i].properties["mag"] > 9) {
+//           color = "DarkMagenta"
+//         }
+//         else if (record.features[i].properties["mag"] > 8) {
+//           color = "Crimson"
+//         }
+//         else if (record.features[i].properties["mag"] > 7) {
+//           color = "Coral"
+//         }
+//         else if (record.features[i].properties["mag"] > 6) {
+//           color = "DarkOrange"
+//         }
+//         else if (record.features[i].properties["mag"] > 5) {
+//           color = "GoldenRod"
+//         }
+//         else if (record.features[i].properties["mag"] > 4) {
+//           color = "Gold"
+//         }
+//         else if (record.features[i].properties["mag"] > 3) {
+//           color = "YellowGreen"
+//         }
+//         else if (record.features[i].properties["mag"] > 2) {
+//           color = "LimeGreen"
+//         }
+//         else if (record.features[i].properties["mag"] > 1) {
+//           color = "MediumTurquoise"
+//         }
+//         else 
+//           color = "DodgerBlue"
+//         }
+// });
+
 
 function createFeatures(earthquakeData) {
 
@@ -47,10 +157,22 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  // var satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}?access_token={accessToken}', {
+    // attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    // maxZoom: 18,
+    var satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    // id: "dark-v10",
+    subdomains:['mt0','mt1','mt2','mt3']
+    // accessToken: API_KEY
+  });
+
+
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
-    "Dark Map": darkmap
+    "Dark Map": darkmap,
+    "Satellite": satellite
   };
 
   // Create overlay object to hold our overlay layer
@@ -58,6 +180,21 @@ function createMap(earthquakes) {
     Earthquakes: earthquakes
   };
 
+  // var magnitude = features.properties.mag
+  // console.log(magnitude)
+  // // function to find the magnitude of a given earthquake
+  // function getMag(data) {
+  //     for (var i = 0; i < limits.length; i++) {
+  //     if (d < limits[i]) {
+  //         return radius[i];
+  //     }
+  //     else if (d > limits[limits.length - 1]) {
+  //         return 10 + radius[limits.length-1];
+  //     }
+  //     }
+  // }
+
+  
     // // limits and circle radius lists
     // var limits = [10, 20, 30, 40, 50, 60, 70, 80, 90, 200];
     // var radius = [6, 8, 10, 12, 14, 16, 17, 18, 19, 20];

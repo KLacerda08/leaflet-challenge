a096d36652539e60d447d6da00c6302744793c9a
KLacerda08 <kaldmr7880@gmail.com>
2021-02-11 21:36:53 -0500

    marker colors working

--- static/js/logic.js
+++ static/js/logic.js
@@ -27,57 +27,109 @@ d3.json(queryUrl, function(record) {
   console.log(record.features[0].geometry["coordinates"][1])
 
 // });
+    // create markerColor(depth);
 
     function markerColor(depth) {
-        var depths = []
-        for (var i = 0; i < record.features.length; i++) {
-          // console.log(Math.max(record.features[i].geometry["coordinates"][2]))
-          if (record.features[i].geometry["coordinates"][2] !== null) {
-          depths.push(parseInt(record.features[i].geometry["coordinates"][2]))
-          }
-        }
+      switch(true){
+        case (depth > 150):
+          return "red";
+        case (depth > 100):
+          return "tomato";
+        case (depth > 50):
+          return "orange";
+        case (depth > 30):
+          return "gold";
+        case (depth > 20):
+          return "yellow";
+        case (depth > 10):
+          return "greenyellow";
+        default:
+          return "lime";
+
+      }
+    }
+
+        // var depths = []
+        // for (var i = 0; i < record.features.length; i++) {
+        //   // console.log(Math.max(record.features[i].geometry["coordinates"][2]))
+        //   if (record.features[i].geometry["coordinates"][2] !== null) {
+        //   depths.push(parseInt(record.features[i].geometry["coordinates"][2]))
+        //   }
+        // }
         // console.log(Math.max.apply(Math, depths))
         // console.log(Math.min.apply(Math, depths))
         // Min = -4, Max = 626
         
-          // Color values:  less than 0, 20, 40, 60, 80, 100, greater than 100
-          for (var i = 0; i < record.features.length; i++) {
-            var color = "";
-            if (record.features[i].geometry["coordinates"][2] > 100) {
-              color = "red"
-            }
-            else if (record.features[i].geometry["coordinates"][2] > 80) {
-              // color = "orange"
-              color = "tomato"
+        // Color values for depths:  less than 10, 20, 30, 50, 100, greater than 150
+        //  lime, greenyellow, yellow, gold, orange, tomato, red
+        // for (var i = 0; i < record.features.length; i++) {
+        // // for (var i = 0; i < depths.length; i++) {
+        //   var color = "";
+        //   if (record.features[i].geometry["coordinates"][2] > 150) {
+        //     color = "red"
+            // console.log(color)
+          // }
+          // else if (record.features[i].geometry["coordinates"][2] > 100) {
+          //   // color = "orange"
+          //   color = "tomato"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] > 50) {
+          //   // color = "yellow"
+          //   color = "orange"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] > 30) {
+          //   // color = "Green"
+          //   // color = "greenyellow"
+          //   color = "gold"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] > 20) {
+          //   // color = "cyan"
+          //   // color = "lime"
+          //   color = "yellow"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] > 10) {
+          //   // color = "blue"
+          //   color = "greenyellow"
+          // } 
+          // else {
+          //   // color = "indigo"
+          //   color = "lime"
+          // } 
 
-            } 
-            else if (record.features[i].geometry["coordinates"][2] > 60) {
-              // color = "yellow"
-              color = "orange"
+          // if (record.features[i].geometry["coordinates"][2] < 10) {
+          //   color = "lime"
+          //   // console.log(color)
+          // }
+          // else if (record.features[i].geometry["coordinates"][2] < 20) {
+          //   // color = "orange"
+          //   color = "greenyellow"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] < 30) {
+          //   // color = "yellow"
+          //   color = "yellow"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] < 40) {
+          //   // color = "Green"
+          //   // color = "greenyellow"
+          //   color = "gold"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] < 50) {
+          //   // color = "cyan"
+          //   // color = "lime"
+          //   color = "orange"
+          // } 
+          // else if (record.features[i].geometry["coordinates"][2] < 100) {
+          //   // color = "blue"
+          //   color = "tomato"
+          // } 
+          // else {
+          //   // color = "indigo"
+          //   color = "red"
+          // } 
+        // }
 
-            } 
-            else if (record.features[i].geometry["coordinates"][2] > 40) {
-              // color = "Green"
-              // color = "greenyellow"
-              color = "gold"
-
-            } 
-            else if (record.features[i].geometry["coordinates"][2] > 20) {
-              // color = "cyan"
-              // color = "lime"
-              color = "yellow"
-
-            } 
-            else if (record.features[i].geometry["coordinates"][2] > 0) {
-              // color = "blue"
-              color = "greenyellow"
-            } 
-            else {
-              // color = "indigo"
-              color = "lime"
-            } 
-          }
-      }
+      // return color;
+    // }
 // });
 
       function createFeatures(earthquakeData) {
@@ -86,23 +138,26 @@ d3.json(queryUrl, function(record) {
         function onEachFeature(record, layer) {
           layer.bindPopup("<strong>Location</strong>: " + record.properties.place +
             "<hr><strong>DateTime</strong>: " + new Date(record.properties.time) + "<hr>" + 
-            "<strong>Magnitude</strong>: " + record.properties.mag);
+            "<strong>Magnitude</strong>: " + record.properties.mag + "; " + 
+            "<strong>Depth (km)</strong>: " + record.geometry["coordinates"][2]);
         }
 // });
           // Create a GeoJSON layer containing the features array on the earthquakeData object
           // Run the onEachFeature function once for each piece of data in the array
           var earthquakes = L.geoJSON(earthquakeData, {
             onEachFeature: onEachFeature,
-            pointToLayer: function (feature, latlng) {
+            pointToLayer: function (record, latlng) {
               var markerType = {
-              radius: 3.5*feature.properties.mag,
-              // fillColor: markerColor(feature.properties.mag),
-              // fillColor: markerColor(depth),
+              radius: 3.5*record.properties.mag,
+              // fillColor: markerColor(depths),
+              // fillColor: color,
+              fillColor: markerColor(record.geometry["coordinates"][2]),
+              // fillColor: color,
               color: "black",
-              // color: markerColor,
+              // fillColor: markerColor
               weight: 1,
-              opacity: 1,
-              fillOpacity: 0.75
+              // opacity: 0.5,
+              fillOpacity: 0.5
               };
               return L.circleMarker(latlng, markerType);
             }
@@ -115,6 +170,13 @@ d3.json(queryUrl, function(record) {
       function createMap(earthquakes) {
 
           // Define map type layers
+          var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+            maxZoom: 18,
+            id: "light-v10",
+            accessToken: API_KEY
+          });
+
           var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
             attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
             tileSize: 512,
@@ -124,22 +186,29 @@ d3.json(queryUrl, function(record) {
             accessToken: API_KEY
           });
 
-          var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          // var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          //   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+          //   maxZoom: 18,
+          //   id: "dark-v10",
+          //   accessToken: API_KEY
+          // });
+        //  Google satellite layer - opted to use leaflet version instead 
+        // var satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+        //   maxZoom: 18,
+        //   subdomains:['mt0','mt1','mt2','mt3']
+        // })
+          var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
             attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
             maxZoom: 18,
-            id: "dark-v10",
+            id: "satellite-streets-v10",
             accessToken: API_KEY
           });
-
-        var satellite = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
-          maxZoom: 18,
-          subdomains:['mt0','mt1','mt2','mt3']
-        })
-        
+          
         // Define a baseMaps object to hold our base layers
         var baseMaps = {
+          "Light Map": lightmap,
           "Street Map": streetmap,
-          "Dark Map": darkmap,
+          // "Dark Map": darkmap,
           "Satellite": satellite
         }
 
@@ -155,9 +224,12 @@ d3.json(queryUrl, function(record) {
           ],
           zoom: 5,
           layers: [
-            streetmap, 
+            // streetmap, 
             // satellite, 
-            earthquakes]
+            lightmap,
+            earthquakes,
+            // markerColor
+          ]
           })
 
     // Create a layer control
